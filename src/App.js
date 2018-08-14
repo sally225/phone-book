2e0e38d839859510d5a953eac089a62b28560dbe
chanmi.park <chanmi.park@cdnetworks.co.kr>
2018-08-14 18:00:58 +0900

    Add phoneInfo, PhoneInfoList

--- src/App.js
+++ src/App.js
@@ -3,18 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 import PhoneForm from './components/PhoneForm'
-
+import PhoneInfoList from './components/PhoneInfoList'
 
 class App extends Component {
+  id = 2
+  state = {
+    information:[
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-1111-1111'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-2222-2222'
+      }
+    ]
+  }
+
   handleCreate = (data) => {
-    console.log(data);
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({id: this.id++, ...data })
+    })
+  }
+  
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+     });
   }
 
   render() {
+    const { information} = this.state
     return (
       <div>	
         <PhoneForm 
           onCreate={this.handleCreate}
+        />
+        <PhoneInfoList 
+          data={this.state.information}
+          onRemove={this.handleRemove}
         />
       </div>
     );
---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class PhoneInfo extends Component {
+  static defaultProps = {
+    info: {
+      name: '이름', 
+      phone: '010-0000-0000',
+      id: 0 
+    }
+  }
+
+  handleRemove = () => {
+      const {info, onRemove} = this.props;
+      onRemove(info.id);
+  }
+
+  render(){
+    const style = {
+      border: '1px solid black',
+      padding: '8px',
+      margin: '8px'
+    };
+
+    const {name, phone, id} = this.props.info;
+
+    return (
+      <div style={style}>
+        <div><b>{name}</b></div>
+        <div>{phone}</div>
+        <button onClick={this.handleRemove}>Del</button>
+      </div>
+    );
+  }
+}
+
+
+export default PhoneInfo;
